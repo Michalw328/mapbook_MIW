6b8cd79b364eed566195edd3e7f8751e46b346ac
Michalw328 <u64828@student.wat.edu.pl>
2025-04-16 16:39:39 +0200

    wip: Adding romove user functionality

--- utils/controller.py
+++ utils/controller.py
@@ -2,8 +2,15 @@ def get_user_info(users_data:list)->None:
     for user in users_data:
         print(f"Twój znajomy {user["name"]} z {user["location"]} opublikował {user["posts"]} postów.")
 
+
 def add_user(users_data:list)->None:
     new_name = input("podaj imię:")
     new_location = input("podaj miejsce:")
     new_posts = input("podaj liczbe postów")
     users_data.append({"name":new_name,"location": new_location,"posts": new_posts})
+
+def remove_user(users_data:list[dict]) -> None:
+    user_name = input("Podaj imię znajomego do usunięcia:")
+    for user in users_data:
+        if user["name"] == user_name:
+            users_data.remove(user)
